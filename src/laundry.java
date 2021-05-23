b845221135e8a94ed715e89eefabc2cf1ea91f7d
unknown <michaelxu17@gmail.com>
2021-05-23 20:19:48 +0700

    resize button on login form

--- src/laundry.java
+++ src/laundry.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Michael
- */
 public class laundry extends javax.swing.JFrame {
 
     /**
